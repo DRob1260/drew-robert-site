617ec4f64ee1825e89290a100f44961b00b20126
Robert, Drew <ddrobe2@ilstu.edu>
2020-09-06 01:08:02 -0500

    fix Urls config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 /* istanbul ignore file */
-const environment = process.env.ENVIRONMENT;
+const environment = process.env.NODE_ENV;
 
 export const Urls = {
   drewRobertApi:
-    environment === "prod"
+    environment === "production"
       ? "https://shielded-spire-10992.herokuapp.com"
       : "http://localhost:8080",
 };
