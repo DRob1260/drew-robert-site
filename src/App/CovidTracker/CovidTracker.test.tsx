a3066e3ff2551e93334425ccc450048329c62ff3
Robert, Drew <ddrobe2@ilstu.edu>
2020-09-06 22:26:50 -0500

    tweak test based on component changes

--- src/App/CovidTracker/CovidTracker.test.tsx
+++ src/App/CovidTracker/CovidTracker.test.tsx
@@ -6,11 +6,20 @@ import { rest } from "msw";
 import { Urls } from "../../config";
 import { setupServer } from "msw/native";
 import { IllinoisRegionData } from "../../mocks/CovidTracker/MockRegionData";
+import { BrowserRouter } from "react-router-dom";
 
 // coverage is pretty low for this component, but a lot of the uncovered code is surrounding the nivo graph component
 
 it("is accessible", async () => {
-  const { container, getByTestId } = render(<CovidTracker />);
+  const { container, getByTestId } = render(
+    <BrowserRouter>
+      <CovidTracker
+        country={"usa"}
+        state={"il"}
+        region={IllinoisRegionData.region.name}
+      />
+    </BrowserRouter>
+  );
   const results = await axe(container);
   const loadingIndicator = getByTestId("loading-indicator-container");
   await waitFor(() => expect(loadingIndicator).not.toBeInTheDocument());
@@ -18,33 +27,68 @@ it("is accessible", async () => {
 });
 
 it("shows a loading indicator while fetching COVID data", async () => {
-  const { getByTestId } = render(<CovidTracker />);
+  const { getByTestId } = render(
+    <BrowserRouter>
+      <CovidTracker
+        country={"usa"}
+        state={"il"}
+        region={IllinoisRegionData.region.name}
+      />
+    </BrowserRouter>
+  );
   const loadingIndicator = getByTestId("loading-indicator-container");
   expect(loadingIndicator).toBeInTheDocument();
   await waitFor(() => expect(loadingIndicator).not.toBeInTheDocument());
 });
 
 describe("region selection", () => {
-  it("has Illinois selected by default", () => {
-    const { getByTestId } = render(<CovidTracker />);
+  it("has the region passed in selected by default", () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <CovidTracker
+          country={"usa"}
+          state={"il"}
+          region={IllinoisRegionData.region.subRegions[0]}
+        />
+      </BrowserRouter>
+    );
     const regionButton = getByTestId("region-button");
-    expect(regionButton).toHaveTextContent("Illinois");
+    expect(regionButton).toHaveTextContent(
+      IllinoisRegionData.region.subRegions[0]
+    );
   });
   it("displays a list of regions when clicked", async () => {
-    const { getByTestId, queryByText } = render(<CovidTracker />);
+    const { getByTestId, queryByText } = render(
+      <BrowserRouter>
+        <CovidTracker
+          country={"usa"}
+          state={"il"}
+          region={IllinoisRegionData.region.name}
+        />
+      </BrowserRouter>
+    );
     const regionButton = getByTestId("region-button");
     expect(queryByText("McLean")).toBeNull();
     fireEvent.click(regionButton);
     await waitFor(() => expect(queryByText("McLean")).not.toBeNull());
   });
-  it("selects the region when it's clicked in the menu", async () => {
-    const { getByTestId, getByText, queryByText } = render(<CovidTracker />);
+  it("navigates to the correct path when a region is clicked", async () => {
+    const { getByTestId, getByText, queryByText } = render(
+      <BrowserRouter>
+        <CovidTracker
+          country={"usa"}
+          state={"il"}
+          region={IllinoisRegionData.region.name}
+        />
+      </BrowserRouter>
+    );
     const regionButton = getByTestId("region-button");
     expect(regionButton).not.toHaveTextContent("McLean");
     fireEvent.click(regionButton);
     await waitFor(() => expect(queryByText("McLean")).not.toBeNull());
     fireEvent.click(getByText("McLean"));
-    expect(regionButton).toHaveTextContent("McLean");
+    const path = document.location.pathname;
+    expect(path).toEqual("/covid/usa/il/McLean");
   });
 });
 
@@ -66,7 +110,13 @@ describe("error handling", () => {
     );
     server.listen();
     const { getByTestId, queryByTestId, queryByText, getByText } = render(
-      <CovidTracker />
+      <BrowserRouter>
+        <CovidTracker
+          country={"usa"}
+          state={"il"}
+          region={IllinoisRegionData.region.name}
+        />
+      </BrowserRouter>
     );
     const regionButton = getByTestId("region-button");
     fireEvent.click(regionButton);
@@ -86,7 +136,15 @@ describe("error handling", () => {
       )
     );
     server.listen();
-    const { queryByTestId } = render(<CovidTracker />);
+    const { queryByTestId } = render(
+      <BrowserRouter>
+        <CovidTracker
+          country={"usa"}
+          state={"il"}
+          region={IllinoisRegionData.region.name}
+        />
+      </BrowserRouter>
+    );
     await waitFor(() => expect(queryByTestId("error-message")).not.toBeNull());
     server.resetHandlers();
     server.close();
