f0bc71d67f3e470967eaf09b106989e504c10b57
drewrobert <drew.robert26@me.com>
2020-09-16 00:23:43 -0500

    add a LocationSelector to be used for CovidTracker

--- src/App/CovidTracker/LocationSelector/LocationSelector.stories.tsx
+++ src/App/CovidTracker/LocationSelector/LocationSelector.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LocationSelector, LocationSelectorProps } from "./LocationSelector";
 import { Story } from "@storybook/react";
 import { SelectedLocations } from "../../../models/CovidTracker/graph/SelectedLocations";
+import { Value } from "../../Inputs/Selector/Selector";
 
 export default {
   title: "LocationSelector",
@@ -12,26 +13,87 @@ const Template: Story<LocationSelectorProps> = (args) => (
   <LocationSelector {...args} />
 );
 
-export const LocationSelectorDefault = Template.bind({});
-LocationSelectorDefault.args = {
-  selectedLocations: {
-    country: undefined,
-    territory: undefined,
-    region: undefined,
+export const LocationSelectorWithValues = Template.bind({});
+LocationSelectorWithValues.args = {
+  countryValues: {
+    values: [
+      {
+        name: "United States of America",
+        key: "unitedstates",
+        value: {
+          name: "United States of America",
+          key: "unitedstates",
+          source: { name: "", apiUrl: "", infoUrl: "" },
+        },
+      },
+    ],
+    current: {
+      name: "United States of America",
+      key: "unitedstates",
+      value: {
+        name: "United States of America",
+        key: "unitedstates",
+        source: { name: "", apiUrl: "", infoUrl: "" },
+      },
+    },
+    setCurrent: (value) => alert(value),
   },
-  setSelectedLocations: (selectedLocations) => {
-    console.log(selectedLocations);
+  territoryValues: {
+    values: [
+      {
+        name: "Illinois",
+        key: "illinois",
+        value: {
+          name: "Illinois",
+          key: "illinois",
+          source: {
+            name: "Illinois Department of Public Health",
+            apiUrl:
+              "https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1",
+            infoUrl: "https://www.dph.illinois.gov/covid19/covid19-statistics",
+          },
+        },
+      },
+    ],
+    current: {
+      name: "Illinois",
+      key: "illinois",
+      value: {
+        name: "Illinois",
+        key: "illinois",
+        source: {
+          name: "Illinois Department of Public Health",
+          apiUrl:
+            "https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1",
+          infoUrl: "https://www.dph.illinois.gov/covid19/covid19-statistics",
+        },
+      },
+    },
+    setCurrent: (value) => alert(value),
   },
-};
-
-export const LocationSelectorWithSelectedLocations = Template.bind({});
-LocationSelectorWithSelectedLocations.args = {
-  selectedLocations: {
-    country: "unitedstates",
-    territory: "illinois",
-    region: "mclean",
-  },
-  setSelectedLocations: (selectedLocations) => {
-    console.log(selectedLocations);
+  regionValues: {
+    values: [
+      {
+        name: "McLean",
+        key: "mclean",
+        value: {
+          name: "Illinois Department of Public Health",
+          apiUrl:
+            "https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1",
+          infoUrl: "https://www.dph.illinois.gov/covid19/covid19-statistics",
+        },
+      },
+    ],
+    current: {
+      name: "McLean",
+      key: "mclean",
+      value: {
+        name: "Illinois Department of Public Health",
+        apiUrl:
+          "https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1",
+        infoUrl: "https://www.dph.illinois.gov/covid19/covid19-statistics",
+      },
+    },
+    setCurrent: (value) => alert(value),
   },
 };
--- src/App/CovidTracker/LocationSelector/LocationSelector.tsx
+++ src/App/CovidTracker/LocationSelector/LocationSelector.tsx
@@ -1,163 +1,38 @@
-import React, { useEffect, useState } from "react";
-import {
-  CircularProgress,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@material-ui/core";
+import React from "react";
 import "./LocationSelector.scss";
-import { LocationClass } from "../../../models/CovidTracker/api/LocationClass";
-import {
-  getCountries,
-  getRegions,
-  getTerritories,
-} from "../../../services/DrewRobertApi";
-import { SelectedLocations } from "../../../models/CovidTracker/graph/SelectedLocations";
+import { Selector, SelectorValues } from "../../Inputs/Selector/Selector";
 
 export interface LocationSelectorProps {
-  changeSelectedCountry: (country?: LocationClass) => void;
-  changeSelectedTerritory: (territory?: LocationClass) => void;
-  changeSelectedRegion: (region?: LocationClass) => void;
+  countryValues: SelectorValues;
+  territoryValues: SelectorValues;
+  regionValues: SelectorValues;
 }
 
 export const LocationSelector: React.FunctionComponent<LocationSelectorProps> = ({
-  changeSelectedCountry,
-  changeSelectedTerritory,
-  changeSelectedRegion,
+  countryValues,
+  territoryValues,
+  regionValues,
 }) => {
-  const [currentCountry, setCurrentCountry] = useState("");
-  const [currentTerritory, setCurrentTerritory] = useState("");
-  const [currentRegion, setCurrentRegion] = useState("");
-  const [countries, setCountries] = useState<LocationClass[]>([]);
-  const [territories, setTerritories] = useState<LocationClass[]>([]);
-  const [regions, setRegions] = useState<LocationClass[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    getCountries().then((result) => {
-      setCountries(result.countries);
-    });
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    if (currentCountry) {
-      setLoading(true);
-      getTerritories(currentCountry).then((result) => {
-        setTerritories(result.territories);
-      });
-      setLoading(false);
-    }
-  }, [currentCountry]);
-
-  useEffect(() => {
-    if (currentTerritory) {
-      setLoading(true);
-      getRegions(currentCountry, currentTerritory).then((result) => {
-        setRegions(result.regions);
-      });
-      setLoading(false);
-    } else {
-      setCurrentRegion("");
-    }
-  }, [currentTerritory, currentCountry]);
-
-  const handleCountryChange = (country?: LocationClass) => {
-    setCurrentCountry(country?.key || "");
-    changeSelectedCountry(country);
-  };
-
-  const handleTerritoryChange = (territory?: LocationClass) => {
-    setCurrentTerritory(territory?.key || "");
-    changeSelectedTerritory(territory);
-  };
-
-  const handleRegionChange = (region?: LocationClass) => {
-    setCurrentRegion(region?.key || "");
-    changeSelectedRegion(region);
-  };
-
   return (
     <div className={"LocationSelector"}>
-      {loading && <CircularProgress />}
-      {!loading && (
-        <div>
-          <FormControl>
-            <InputLabel id={"country-label"}>Country</InputLabel>
-            <Select labelId={"country-label"} value={currentCountry}>
-              <MenuItem
-                value={""}
-                onSelect={() => {
-                  handleCountryChange(undefined);
-                }}
-              >
-                none
-              </MenuItem>
-              {countries.map((country) => {
-                return (
-                  <MenuItem
-                    value={country.key}
-                    key={`LocationSelector-${country.key}`}
-                    onSelect={() => handleCountryChange(country)}
-                  >
-                    {country.name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-          {currentCountry && (
-            <FormControl>
-              <InputLabel id={"territory-label"}>Territory</InputLabel>
-              <Select labelId={"territory-label"} value={currentTerritory}>
-                <MenuItem
-                  value={""}
-                  onSelect={() => handleTerritoryChange(undefined)}
-                >
-                  none
-                </MenuItem>
-                {territories.map((territory) => {
-                  return (
-                    <MenuItem
-                      value={territory.key}
-                      key={`LocationSelector-${territory.key}`}
-                      onSelect={() => handleTerritoryChange(territory)}
-                    >
-                      {territory.name}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-          )}
-          {currentTerritory && (
-            <FormControl>
-              <InputLabel id={"region-label"}>Region</InputLabel>
-              <Select labelId={"region-label"} value={currentRegion}>
-                <MenuItem
-                  value={""}
-                  onSelect={() => handleRegionChange(undefined)}
-                >
-                  none
-                </MenuItem>
-                {regions.map((region) => {
-                  return (
-                    <MenuItem
-                      value={region.key}
-                      key={`LocationSelector-${region.key}`}
-                      onSelect={() => handleRegionChange(region)}
-                    >
-                      {region.name}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-          )}
-        </div>
-      )}
+      <Selector
+        selectorValues={countryValues}
+        selectorConfiguration={{
+          label: "Country",
+        }}
+      />
+      <Selector
+        selectorValues={territoryValues}
+        selectorConfiguration={{
+          label: "Territory",
+        }}
+      />
+      <Selector
+        selectorValues={regionValues}
+        selectorConfiguration={{
+          label: "Region",
+        }}
+      />
     </div>
   );
 };
