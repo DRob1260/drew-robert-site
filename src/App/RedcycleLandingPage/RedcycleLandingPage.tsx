f3e5e6ccc841209ca7e2ede22134caed9263ed80
Robert, Drew <ddrobe2@ilstu.edu>
2020-10-10 19:54:52 -0500

    remove unused import

--- src/App/RedcycleLandingPage/RedcycleLandingPage.tsx
+++ src/App/RedcycleLandingPage/RedcycleLandingPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, ButtonGroup, Link } from "@material-ui/core";
+import { Button, ButtonGroup } from "@material-ui/core";
 import "./RedcycleLandingPage.scss";
 
 export const RedcycleLandingPage: React.FunctionComponent = () => {
