ae1a80018b7e6478287e4837e78168f969b02ac6
drewrobert <drew.robert26@me.com>
2020-12-10 13:55:45 -0600

    update urls for redcycle admin page

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ export const Urls = {
   covidApi: local ? "http://localhost:2601/api/covid" : "/api/covid",
   redcycleUi: local ? "http://localhost:2601/redcycle" : "/redcycle",
   redcycleAdmin: local
-    ? "http://localhost:8000/redcycle/admin"
-    : "/redcycle/admin",
+    ? "http://localhost:8000/redcycle/admin/"
+    : "/redcycle/admin/",
 };
