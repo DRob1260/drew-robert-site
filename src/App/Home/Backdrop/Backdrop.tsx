3216531ee75ef45af646e9053eb7d9e5dda704cd
drewrobert <drew.robert26@me.com>
2020-12-20 11:55:34 -0600

    render image smoothly

--- src/App/Home/Backdrop/Backdrop.tsx
+++ src/App/Home/Backdrop/Backdrop.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Backdrop as MaterialBackdrop } from "@material-ui/core";
 import { getFlickrUserPhotoUrls } from "../../../services/DrewRobertApi/Gallery";
 import { randomValue } from "../../../utilities/ArrayUtilities";
 import "./Backdrop.scss";
 
 export const Backdrop: React.FunctionComponent = () => {
   const [photoUrl, setPhotoUrl] = useState("");
-  const [applyShader, setApplyShader] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getFlickrUserPhotoUrls("69728079%40N03").then((photoUrls) => {
@@ -23,11 +24,17 @@ export const Backdrop: React.FunctionComponent = () => {
               data-testid={"backdrop-image"}
               src={photoUrl}
               alt={"from my flickr feed"}
+              onLoad={() => {
+                setLoading(false);
+              }}
             />
           )}
+          <MaterialBackdrop open={loading} transitionDuration={1000}>
+            <div id={"image-placeholder"} />
+          </MaterialBackdrop>
         </div>
         <div id={"greeting-container"}>
-          <h1 id={"greeting"}>Hello World!</h1>
+          {!loading && <h1 id={"greeting"}>Hello World!</h1>}
         </div>
       </div>
     </div>
