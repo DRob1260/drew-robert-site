9fbd37dc38758ed1bd7a36fd817210585af5dc07
drewrobert <drew.robert26@me.com>
2020-12-27 18:06:42 -0600

    add additional tests

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -2,40 +2,78 @@ import React from "react";
 import { axe } from "jest-axe";
 import { render, fireEvent, waitFor } from "@testing-library/react";
 import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 it("should be accessible", async () => {
-  const { container } = render(<App />);
+  const { container, queryByText } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  // wait for CovidTrackerPreviewCard to finish loading
+  await waitFor(() => expect(queryByText("Illinois Cases")).not.toBeNull());
   const results = await axe(container);
   expect(results).toHaveNoViolations();
 });
 
-it("renders the navigator", () => {
-  const { queryByTestId } = render(<App />);
+it("renders the navigator", async () => {
+  const { queryByTestId, queryByText } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  // wait for CovidTrackerPreviewCard to finish loading
+  await waitFor(() => expect(queryByText("Illinois Cases")).not.toBeNull());
   expect(queryByTestId("Navigator")).not.toBeNull();
 });
 
-it("renders the footer", () => {
-  const { queryByTestId } = render(<App />);
+it("renders the footer", async () => {
+  const { queryByTestId, queryByText } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  // wait for CovidTrackerPreviewCard to finish loading
+  await waitFor(() => expect(queryByText("Illinois Cases")).not.toBeNull());
   expect(queryByTestId("Footer")).not.toBeNull();
 });
+describe("rendering pages", () => {
+  it("renders the home page by default", async () => {
+    const { queryByTestId, queryByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    // wait for CovidTrackerPreviewCard to finish loading
+    await waitFor(() => expect(queryByText("Illinois Cases")).not.toBeNull());
+    expect(queryByTestId("Home")).not.toBeNull();
+  });
 
-it("renders the home page by default", () => {
-  const { queryByTestId } = render(<App />);
-  expect(queryByTestId("Home")).not.toBeNull();
-});
+  it("renders the Redcycle landing page", async () => {
+    const { queryByTestId, getByTestId, queryByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    fireEvent.click(getByTestId("projects-menu"));
+    fireEvent.click(getByTestId("redcycle-link"));
+    await waitFor(() =>
+      expect(queryByTestId("RedcycleLandingPage")).not.toBeNull()
+    );
+  });
 
-it("renders the covid metrics tracker project", async () => {
-  const { queryByTestId, getByTestId, getByText } = render(<App />);
-  fireEvent.click(getByTestId("projects-menu"));
-  fireEvent.click(getByText("COVID-19 Metrics Tracker"));
-  await waitFor(() => expect(queryByTestId("CovidTracker")).not.toBeNull());
-});
-
-it("renders the Redcycle landing page", async () => {
-  const { queryByTestId, getByTestId, getByText } = render(<App />);
-  fireEvent.click(getByTestId("projects-menu"));
-  fireEvent.click(getByText("Redcycle"));
-  await waitFor(() =>
-    expect(queryByTestId("RedcycleLandingPage")).not.toBeNull()
-  );
+  it("renders the covid metrics tracker project", async () => {
+    const { queryByTestId, getByTestId, queryByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    fireEvent.click(getByTestId("projects-menu"));
+    fireEvent.click(getByTestId("covid-metrics-link"));
+    await waitFor(() => {
+      expect(queryByTestId("CovidTracker")).not.toBeNull();
+      expect(queryByText("United States of America")).not.toBeNull();
+      expect(queryByText("Illinois")).not.toBeNull();
+    });
+  });
 });
--- src/App/CovidTracker/CovidTrackerPreviewCard.test.tsx
+++ src/App/CovidTracker/CovidTrackerPreviewCard.test.tsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { axe } from "jest-axe";
 import { render, waitFor } from "@testing-library/react";
-import { Home } from "./Home";
+import { CovidTrackerPreviewCard } from "./CovidTrackerPreviewCard";
 import { BrowserRouter } from "react-router-dom";
 
 it("should be accessible", async () => {
   const { container, queryByText } = render(
     <BrowserRouter>
-      <Home />
+      <CovidTrackerPreviewCard />
     </BrowserRouter>
   );
   // wait for CovidTrackerPreviewCard to finish loading
   await waitFor(() => expect(queryByText("Illinois Cases")).not.toBeNull());
-  const results = await axe(container);
-  expect(results).toHaveNoViolations();
+  expect(await axe(container)).toHaveNoViolations();
 });
 
-it("displays Projects", async () => {
-  const { queryByTestId, queryByText } = render(
+it("displays the desired information", async () => {
+  const { queryByText, queryByTestId } = render(
     <BrowserRouter>
-      <Home />
+      <CovidTrackerPreviewCard />
     </BrowserRouter>
   );
-  // wait for CovidTrackerPreviewCard to finish loading
   await waitFor(() => expect(queryByText("Illinois Cases")).not.toBeNull());
-  expect(queryByTestId("Projects")).not.toBeNull();
+  expect(queryByText("COVID-19 Metrics Tracker")).not.toBeNull();
+  expect(queryByTestId("visit-project-button")).not.toBeNull();
 });
