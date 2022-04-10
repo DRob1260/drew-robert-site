ff74965809bcf3b3072ad67e717ea4ca8cfb5eb1
DRob1260 <drew.robert26@me.com>
2022-04-10 15:11:40 -0500

    switch to using MyTable for MME Orders page

--- src/App/MME/MME.tsx
+++ src/App/MME/MME.tsx
@@ -27,31 +27,28 @@ export const MME: React.FunctionComponent = () => {
 
   return (
     <div className={"MME"}>
-      <h1>Mustang Mach-E</h1>
+      <h1>Mach-E Forum Orders</h1>
       <p>
-        I ordered a Mustang Mach-E and am (impatiently) waiting on updates for
-        its production and delivery. To pass my time, I'm building some tools to
-        help keep an eye on the progress of orders!
+        I ordered a Mustang Mach-E last year and built this tool to help monitor
+        progress of other orders to predict when my order might be built. These
+        Mach-E orders are pulled live from the data in the{" "}
+        <a
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+          href={
+            "https://www.macheforum.com/site/threads/submitted-orders-tracking-list-and-stats-enter-yours.924/"
+          }
+        >
+          order tracker
+        </a>{" "}
+        in the Mach-E Forum. I find it useful to apply sorting and filtering to
+        the values to narrow down analysis.
       </p>
       <div className={"mach-e-forum-orders"}>
         <div className={"mach-e-forum-orders-header"}>
-          <h2>Mach-E Forum Orders</h2>
           {loading && <CircularProgress size={"1.25em"} />}
         </div>
-        <p>
-          These Mach-E orders are pulled live from the data in the{" "}
-          <a
-            target={"_blank"}
-            rel={"noopener noreferrer"}
-            href={
-              "https://www.macheforum.com/site/threads/submitted-orders-tracking-list-and-stats-enter-yours.924/"
-            }
-          >
-            order tracker
-          </a>{" "}
-          in the Mach-E Forum. I find it useful to apply sorting and filtering
-          to the values to narrow down analysis.
-        </p>
+        <p></p>
         <div
           style={{
             marginBottom: "1em",
@@ -142,7 +139,6 @@ export const MME: React.FunctionComponent = () => {
             ]}
           />
         </div>
-        <MmeForumOrders mmeForumOrders={mmeForumOrders} />
       </div>
       <Snackbar open={error}>
         <Alert severity={"warning"}>
